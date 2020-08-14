21cb0539dd1d6983dbfece0b9e6300608acb44f0
eric2523@gmail.com <eric2523@gmail.com>
2020-08-13 21:33:20 -0700

    Add functionality to instance methods

--- w3d5/diy_adts.rb
+++ w3d5/diy_adts.rb
@@ -19,17 +19,23 @@ class Stack
 end
 
 class Queue
+    attr_reader :queue 
+
+    def initialize
+        @queue = []
+    end
+
     def enqueue(el)
-        
+        queue.unshift(el)
     end
 
     def dequeue
-        
+        queue.shift
     end
 
     def peek
-        
+        queue.first
     end
-
 end
+
 
