b826b15c7d8f0cff3d68cc0309c770837c3e5bcf
eric2523@gmail.com <eric2523@gmail.com>
2020-09-07 14:43:54 -0700

    Add password_digest setter and method

--- w7d1/app_name/app/models/user.rb
+++ w7d1/app_name/app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
     attr_reader :password
 
-    validates :username :session_token, presence: true 
+    validates :username, :session_token, presence: true 
     validates :password_digest, presence: { message: 'Password can''t be blank'}
     validates :password, length: {minimum: 6, allow_nil: true}
 
     def password=(password)
         @password = password
-        password_digest = BCrypt::Password.create(password)
+        self.password_digest = BCrypt::Password.create(password)
     end
 end
