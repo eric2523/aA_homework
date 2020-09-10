aafae956ea6e5a5ec55d766ca0c27a00aa14a4b5
eric2523@gmail.com <eric2523@gmail.com>
2020-09-09 23:14:40 -0700

    Create pending specs for ::find_by_credentials

--- w7d3/music_app/spec/models/user_spec.rb
+++ w7d3/music_app/spec/models/user_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+    let(:user) {User.create(email: 'aaa@aa.io', password: 'password123')}
+
     describe "User should have email" do
       it { should validate_presence_of(:email)}
     end
@@ -14,14 +16,28 @@ RSpec.describe User, type: :model do
     end
 
     describe "#is_password?" do
-      let(:user) {User.create(email: 'aaa@aa.io', password: 'password123')}
-
       it "should return true if password matches" do 
         expect(user.is_password?("password123")).to eq(true)
       end
       it "should return false if password does not match" do
         expect(user.is_password?("password567")).to eq(false)
       end
+    end
 
+    describe "#reset_session_token!" do 
+      it "should reset the user's session token" do 
+        old = user.session_token 
+        expect(user.reset_session_token!).not_to eq(old)
+      end
+
+      it "should return the user's session token" do 
+        expect(user.reset_session_token!).to eq(user.session_token)
+      end
+    end
+
+    describe "::find_by_credentials" do 
+      it "should accept an email and password as arguments" 
+      it "should return the user if user is found"
+      it "should return nil if user is not found"
     end
 end
