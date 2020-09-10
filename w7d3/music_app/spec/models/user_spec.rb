e4f030e020d0f3930e94df6b0045f8d8af447329
eric2523@gmail.com <eric2523@gmail.com>
2020-09-09 23:04:25 -0700

    Create is_password? rspec test true or false

--- w7d3/music_app/spec/models/user_spec.rb
+++ w7d3/music_app/spec/models/user_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    describe "User should have email" do
+      it { should validate_presence_of(:email)}
+    end
+
+    describe "User should have password_digest" do
+      it { should validate_presence_of(:password_digest)}
+    end
+
+    describe "User's password should be longer than 6 characters" do 
+      it { should validate_length_of(:password).is_at_least(6)}
+    end
+
+    describe "#is_password?" do
+      let(:user) {User.create(email: 'aaa@aa.io', password: 'password123')}
+
+      it "should return true if password matches" do 
+        expect(user.is_password?("password123")).to eq(true)
+      end
+      it "should return false if password does not match" do
+        expect(user.is_password?("password567")).to eq(false)
+      end
+
+    end
 end
