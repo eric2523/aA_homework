9a76eb1bdeb2ffb90527720b7376df027cf7c7db
eric2523@gmail.com <eric2523@gmail.com>
2020-09-15 23:53:27 -0700

    Create Intro JS problems and create madLib function

---
+++ w8d3/IntroToJS.js
@@ -0,0 +1,8 @@
+
+//------madLib----------
+function madLib(verb, adj, noun) {
+    let sentence = `We shall ${verb} the ${adj} ${noun}`
+    console.log(sentence);
+}
+
+madLib('make', 'best', 'guac');
