fef1aeb527305c93981cee1a60a4c6d8e8ee9e7e
eric2523@gmail.com <eric2523@gmail.com>
2020-09-16 00:11:50 -0700

    Add isSusbtring function

--- w8d3/IntroToJS.js
+++ w8d3/IntroToJS.js
@@ -5,4 +5,18 @@ function madLib(verb, adj, noun) {
     console.log(sentence);
 }
 
-madLib('make', 'best', 'guac');
+// madLib('make', 'best', 'guac');
+
+//----isSubstring------
+function isSubstring(searchString, subString) {
+    let sentence = searchString.split(' ')
+    for(let i = 0; i < sentence.length; i++) {
+        if (subString === sentence[i]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+// console.log(isSubstring("time to program", "time"));
+// console.log(isSubstring("Jump for joy", "joys"));
