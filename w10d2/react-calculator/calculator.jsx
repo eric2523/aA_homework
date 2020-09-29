3955ddc3c0c77cc3d6e6bb8f2117dd41bb98572d
eric2523@gmail.com <eric2523@gmail.com>
2020-09-28 21:56:42 -0700

    Add operation buttons and functionality

--- w10d2/react-calculator/calculator.jsx
+++ w10d2/react-calculator/calculator.jsx
@@ -7,19 +7,45 @@ class Calculator extends React.Component {
     this.state = { num1: "", num2: "", result: 0 };
     this.setNum1 = this.setNum1.bind(this);
     this.setNum2 = this.setNum2.bind(this);
+    this.add = this.add.bind(this);
+    this.subtract = this.subtract.bind(this);
+    this.multiply = this.multiply.bind(this);
+    this.divide = this.divide.bind(this);
   }
 
   // your code here
   setNum1(e) {
-    e.preventDefault();
     const num1 = e.target.value ? parseInt(e.target.value) : ""
     this.setState({ num1 })
   }
 
   setNum2(e) {
-    e.preventDefault();
     const num2 = e.target.value ? parseInt(e.target.value) : "";
     this.setState({ num2 })
+  }
+
+  add(e) {
+    e.preventDefault();
+    const result = this.state.num1 + this.state.num2
+    this.setState({ result })
+  }
+
+  subtract(e) {
+    e.preventDefault();
+    const result = this.state.num1 - this.state.num2;
+    this.setState({ result })
+  }
+
+  multiply(e) {
+    e.preventDefault();
+    const result = this.state.num1 * this.state.num2;
+    this.setState({ result })
+  }
+
+  divide(e) {
+    e.preventDefault();
+    const result = this.state.num1 / this.state.num2;
+    this.setState({ result })
   }
 
   render() {
@@ -29,6 +55,11 @@ class Calculator extends React.Component {
         <h1>{ result }</h1> 
         <input onChange={ this.setNum1 } value={ num1 }/>
         <input onChange={ this.setNum2 } value={ num2 }/>
+        <br />
+        <button onClick={ this.add }>+</button>
+        <button onClick={ this.subtract }>-</button>
+        <button onClick={ this.multiply }>*</button>
+        <button onClick={ this.divide }>/</button>
       </div>
     );
   }
